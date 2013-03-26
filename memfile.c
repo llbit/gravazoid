3e1086100e2f0fe084719d856f9a7548c4fb9b1d
Jesper Öqvist <jesper@llbit.se>
2013-03-27 00:17:42 +0100

    Use fmemopen if available

--- memfile.c
+++ memfile.c
@@ -2,8 +2,12 @@
 
 FILE* mem_to_file(void* buf, size_t size)
 {
+#ifdef __WIN32__
 	FILE* fp = tmpfile();
 	fwrite(buf, size, 1, fp);
 	rewind(fp);
 	return fp;
+#else
+	return fmemopen(buf, size, "r");
+#endif
 }
